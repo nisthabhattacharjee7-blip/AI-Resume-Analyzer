6c547c0bb62dae02fc5d3700aeff143b0754dd80
Nistha <nisthabhattacharjee7@gmail.com>
2026-05-19 23:31:33 +0530

    Improved UI and ATS dashboard

--- app.py
+++ app.py
@@ -34,25 +34,51 @@ uploaded_file = st.file_uploader(
 
 job_description = st.text_area("Paste job description here")
 
+# main logic
 if uploaded_file is not None:
     extracted_text = extract_text_from_pdf(uploaded_file)
     resume_skills = extract_skills(extracted_text)
     jb_skills = extract_skills(job_description)
     ats_score, matched_skills, missing_skills = calculate_ats_score(resume_skills, jb_skills)
-
+    
+    # success message 
     st.success("Resume analyzed successfully!")
-
+    
+    # ATS score
     st.subheader("ATS Score")
-    st.metric("Match Percentage", f"{ats_score:.2f}%")
-
+    st.progress(ats_score/100)
+    st.metric(
+        label = "Match Percentage",
+        value = f"{ats_score:.2f}%")
+    
+    # detected skills
     st.subheader("Detected resume skills")
-    st.write(resume_skills)
-
+    if resume_skills:
+        st.success(f"Detected {len(resume_skills)} skills in resume")
+    else:
+        st.warning("No skills detected in resume")
+    
+    # Matched skills 
     st.subheader("Matched skills")
-    st.write(matched_skills)
-
+    if matched_skills:
+        st.success(f"Matched {len(matched_skills)} skills in job description")
+    else:
+        st.warning("No matched skills found in job description")
+    
+    # Missing skills
     st.subheader("Missing skills")
-    st.write(missing_skills)
+    if missing_skills:
+        st.warning(f"Missing {len(missing_skills)} skills in resume")
+    else:
+        st.success("All required skills are present in the resume")
 
+    # Skill Dataframe
+    st.subheader("Skill Match Table")
 
+    df = pd.DataFrame({
+        "Matched Skills": pd.Series(matched_skills),
+        "Missing Skills": pd.Series(missing_skills)
+    })
+    st.dataframe(df)
+    
 
