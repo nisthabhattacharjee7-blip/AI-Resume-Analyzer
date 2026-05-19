326ed5dc96ff7a94503ede420ca4614b71098d63
Nistha <nisthabhattacharjee7@gmail.com>
2026-05-19 12:57:55 +0530

    added sidebar

--- app.py
+++ app.py
@@ -1,7 +1,34 @@
 import streamlit as st
+import pandas as pd
 from parser import extract_text_from_pdf
 from skills import extract_skills
 from scorer import calculate_ats_score
+
+
+# Page config
+st.set_page_config(
+    page_title="AI Resume Analyzer", 
+    page_icon="📄", 
+    layout="centered")
+
+# Title and markdown 
+st.title("📄AI Resume Analyzer")
+st.markdown("Analyze resume ATS compatibility with job descriptions.")
+
+# Sidebar
+st.sidebar.header("About")
+st.sidebar.info(
+        """
+    AI Resume Analyzer built using:
+    
+    - Python
+    - Streamlit
+    - PyPDF2
+    - NLP-style skill matching
+    """
+)
+
+
 
 st.title("AI Resume Analyzer")
 uploaded_file = st.file_uploader("Upload resume PDF", type=["pdf"])
